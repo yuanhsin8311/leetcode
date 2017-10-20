99dad2364316bddd3012d771e2f1fad2393254fd
yuanhsin huang <yuanhsin8311@gmail.com>
2017-10-19 19:17:16 -0700

    add file

--- twoSum.py
+++ twoSum.py
@@ -9,4 +9,30 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 """
+# Method 1: O(n)
+class Solution(object):
+	def twoSum(self, nums, target):
+		for i in xrang(len(nums)):
+			for j in xrange(i+1,len(nums)):
+				if target - nums[j] == nums[i]:
+					return [i,j]
 
+# Method 2: heap table / O(1)
+class Solution(object):
+	def twoSum(self, nums, target):
+		dict = {}
+		for i, num in enumerate(nums):
+			dict[num] = i
+		for i, num in enumerate(nums):
+			if target - num in dict and dict[target-num] != i:
+				return (i,dict[target-num])
+
+## 優化解法
+class Solution(object):
+	def twoSum(self, nums, target):
+		dict = {}
+		for i, num in enumerate(nums):
+			if target - num in dict:
+				return (i, dict[target-num])
+			else:
+				dict[num] = i
